091341d15f6767356a9e9a35a8590efe6d6ca2f1
Pablo Ibanez <me@pabloibanez.com>
2017-04-11 16:07:43 +0100

    Phylogram is created from TS class now

---
+++ src/assets/phylogramOptions.ts
@@ -0,0 +1,8 @@
+export class PhylogramOptions {
+    width: number;
+    height?: number;
+    labelHeight?: number;
+    skipLabels: boolean;
+    ratioLengthPropertyName: string;
+    namePropertyName: string;
+}
